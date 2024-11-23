7513ab06a6c57d46a122f1a1dc8534ae0413ef17
cyh <1909625265@qq.com>
2024-11-24 05:17:24 +0800

    modify _halt

--- am/src/cyh/isa/riscv/trm.c
+++ am/src/cyh/isa/riscv/trm.c
@@ -17,8 +17,13 @@ void _putc(char ch) {
   __am_uartlite_putchar(ch);
 }
 
+#define EXIT_POS 0x40000000
+#define EXIT_CODE 0xdeadbeef
+
 void _halt(int code) {
-  __asm__ volatile("mv a0, %0; .word 0x0005006b" : :"r"(code));
+  __asm__ volatile("mv a0, %0" : :"r"(code));
+  // exit
+  __asm__ volatile("li t0, %0; li t1, %1; sw t1, 0(t0)" : : "i"(EXIT_POS), "i"(EXIT_CODE));
 
   // should not reach here during simulation
   printf("Exit with code = %d\n", code);
